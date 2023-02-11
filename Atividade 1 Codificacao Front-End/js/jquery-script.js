b72fdb1a46949b6a674a08533f64e7741b60388d
isaacQ13 <isaac.quinto@ufpe.br>
2023-02-11 17:39:03 -0300

    Ultimo Commit da atividade do senai

--- Atividade 1 Codificacao Front-End/js/jquery-script.js
+++ Atividade 1 Codificacao Front-End/js/jquery-script.js
@@ -1,1 +1,8 @@
 //efeito de esconder formulário de cadastro
+$(document).ready(function() {
+    $("#botao-cadastrar").click(function() {
+        $("#form-cadastrar").slideToggle("slow");
+        $("#section-login").slideToggle("slow");
+        $("#botao-cadastrar").hide();
+    });
+});
--- Atividade 1 Codificacao Front-End/js/script.js
+++ Atividade 1 Codificacao Front-End/js/script.js
@@ -1,5 +1,28 @@
 //efeito do botão voltar ao Topo
-
+function topo() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+}
 //Validação de Login
+function login() {
+    var logado = 0;
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+
+    if (usuario == 'admin' && senha == '12345') {
+        window.location = 'index.html'
+        logado = 1;
+    }
+    if (logado == 0) {
+        alert("Acesso Negado. Dados Incorretos")
+    }
+}
 
 //Ativar alert no botão cadastrar
+function cadastro() {
+    alert("Cadastro feito com sucesso.");
+    window.location.href = "index.html";
+}
